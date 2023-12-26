f944b0ec52f646a7d35144f2ad6b27dac6ad2dd3
thizzmoe <moekaybusiness@gmail.com>
2023-12-25 19:00:40 -0800

    f

---
+++ main.py
@@ -0,0 +1,6 @@
+import streamlit as st
+###
+st.set_page_config(page_title="Free File Converter", page_icon=":tada:", layout="wide")
+
+# header
+st.subheader("This is a free converter for ALL file types!")
